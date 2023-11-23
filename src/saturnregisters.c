64d137ade42e3f07d384aa12389d402f1923beff
laurencebarker <laurence@nicklebyhouse.co.uk>
2023-11-23 19:52:51 +0000

    added ATU bit into IO6 position

--- src/saturnregisters.c
+++ src/saturnregisters.c
@@ -1793,12 +1793,14 @@ unsigned int GetADCOverflow(void) {
 //
 // GetUserIOBits(void)
 // return the user input bits
-// returns IO4 in LSB, IO8 in bot 3
+// returns IO4 in LSB, IO5 in bit 1, ATU bit in bit 2 & IO8 in bit 3
 //
-unsigned int GetUserIOBits(void) {
-  unsigned int Result = 0;
-  Result = ((GStatusRegister >> VUSERIO4) & 0b1111);                       // get usder input 4/5/6/8
-  return Result;
+unsigned int GetUserIOBits(void)
+{
+    unsigned int Result = 0;
+    Result = ((GStatusRegister >> VUSERIO4) & 0b1011);                       // get usder input 4/5/-/8
+    Result |= ((GStatusRegister >> 7) & 0b0100);                             // get ATU bit into IO6 location
+    return Result;
 }
 
 //
