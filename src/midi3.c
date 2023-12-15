e46b7a2732604029fccceb1340c7812db8152c75
c vw <dl1ycf@darc.de>
2023-12-15 17:01:57 +0100

    MIDI update for very fast firing VFO knobs.

--- src/midi3.c
+++ src/midi3.c
@@ -28,21 +28,73 @@
 #include <gtk/gtk.h>
 
 #include "actions.h"
+#include "message.h"
 #include "midi.h"
 
+struct _vfo_timer {
+  int action;
+  int val;
+  guint timeout;
+};
+
+typedef struct _vfo_timer VFO_TIMER;
+
+VFO_TIMER vfo_timer =  {VFO,  0, 0};
+VFO_TIMER vfoa_timer = {VFOA, 0, 0};
+VFO_TIMER vfob_timer = {VFOB, 0, 0};
+
+static int vfo_timeout_cb(gpointer data) {
+  VFO_TIMER *timer = (VFO_TIMER *)data;
+  t_print("%s: action=%d val=%d\n",__FUNCTION__, timer->action, timer->val);
+  schedule_action(timer->action, RELATIVE, timer->val);
+  timer->timeout = 0;
+  timer->val = 0;
+  return FALSE;
+}
+
 void DoTheMidi(int action, enum ACTIONtype type, int val) {
-  //t_print("%s: action=%d type=%d val=%d\n",__FUNCTION__,action,type,val);
   switch (type) {
   case MIDI_KEY:
+    t_print("%s: action=%d val=%d\n",__FUNCTION__,action,val);
     schedule_action(action, val ? PRESSED : RELEASED, 0);
     break;
 
   case MIDI_KNOB:
+    t_print("%s: action=%d val=%d\n",__FUNCTION__,action,val);
     schedule_action(action, ABSOLUTE, val);
     break;
 
   case MIDI_WHEEL:
-    schedule_action(action, RELATIVE, val);
+    //
+    // There are "big wheels" at various MIDI consoles that can produce MIDI events
+    // with rather high frequency, and these are usually used for VFO, VFOA, VFOB
+    // Therefore, these events are filtererd out here in a way that several such events
+    // lead to a single scheduled action with a "consolidated" value. The idea is, that
+    // turning the VFO knob fast will only generate one VFO update every 100 msec.
+    //
+    switch (action) {
+    case VFOA:
+      vfoa_timer.val += val;
+      if (vfoa_timer.timeout == 0) {
+        vfoa_timer.timeout = g_timeout_add(100, vfo_timeout_cb, &vfoa_timer);
+      }
+      break;
+    case VFOB:
+      vfob_timer.val += val;
+      if (vfob_timer.timeout == 0) {
+        vfob_timer.timeout = g_timeout_add(100, vfo_timeout_cb, &vfob_timer);
+      }
+      break;
+    case VFO:
+      vfo_timer.val += val;
+      if (vfo_timer.timeout == 0) {
+        vfo_timer.timeout = g_timeout_add(100, vfo_timeout_cb, &vfo_timer);
+      }
+      break;
+    default:
+      t_print("%s: action=%d val=%d\n",__FUNCTION__,action,val);
+      schedule_action(action, RELATIVE, val);
+    }
     break;
 
   default:
