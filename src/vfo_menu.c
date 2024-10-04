d03bcec87b370932e95ad17baa975a81f76ed5c5
c vw <dl1ycf@darc.de>
2024-10-04 10:02:43 +0200

    Enlarged buttons in VFO menu to make this touch screen friendly.

--- src/vfo_menu.c
+++ src/vfo_menu.c
@@ -179,10 +179,10 @@ void vfo_menu(GtkWidget *parent, int id) {
   g_signal_connect (dialog, "destroy", G_CALLBACK (close_cb), NULL);
   GtkWidget *content = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
   GtkWidget *grid = gtk_grid_new();
-  gtk_grid_set_column_homogeneous(GTK_GRID(grid), FALSE);
+  gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
   gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
-  gtk_grid_set_column_spacing (GTK_GRID(grid), 4);
-  gtk_grid_set_row_spacing (GTK_GRID(grid), 4);
+  gtk_grid_set_column_spacing (GTK_GRID(grid), 8);
+  gtk_grid_set_row_spacing (GTK_GRID(grid), 8);
   GtkWidget *close_b = gtk_button_new_with_label("Close");
   gtk_widget_set_name(close_b, "close_button");
   g_signal_connect (close_b, "button-press-event", G_CALLBACK(close_cb), NULL);
@@ -195,7 +195,7 @@ void vfo_menu(GtkWidget *parent, int id) {
 
   for (i = 0; i < 16; i++) {
     btn[i] = gtk_button_new_with_label(btn_labels[i]);
-    gtk_widget_set_name(btn[i], "close_button");
+    gtk_widget_set_name(btn[i], "medium_button");
     gtk_widget_show(btn[i]);
 
     if (i == 15) {
