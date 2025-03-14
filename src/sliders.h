b3c023a2b8751fe09f0cea768b62a76209caf6af
c vw <dl1ycf@darc.de>
2025-03-14 12:13:06 +0100

    Removed declaration of sliders_update() since this function no longer exists (tnx Heiko).

--- src/sliders.h
+++ src/sliders.h
@@ -44,7 +44,6 @@ extern void show_filter_shift(int rx, int value);
 extern void set_attenuation_value(double attenuation);
 extern GtkWidget *sliders_init(int my_width, int my_height);
 
-extern void sliders_update(void);
 
 extern void set_squelch(RECEIVER *rx);
 
